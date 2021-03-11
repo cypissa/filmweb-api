fc632d4ee347e30190b1cdf723ecaed32665404b
Cyprian <cypiszzz@gmail.com>
2021-03-11 08:53:35 +0100

    Add test for retrieving user films

--- tests/filmweb_api_test.py
+++ tests/filmweb_api_test.py
@@ -21,15 +21,21 @@ def test_logout(mocker):
     request_session.cookies.clear.assert_called_once()
 
 
+def _login(sut, requests_mock):
+    requests_mock.post(
+        API_HOST + '?version=1.0&appId=android&methods=login%20%5B%22test_username%22%2C%20%22test_password%22%2C%201%5D%0A&signature=1.0,cc93095163a83e913c8346a734f9ddc8',
+        text='ok\'b\n["test_username", "avatar.gif", "John Smith", 8889, "M"]',
+        cookies={'_art_userid': 1231432523})
+
+    sut.login()
+
+
 def test_login(requests_mock):
     """ Login test - success """
 
-    requests_mock.post(API_HOST + '?version=1.0&appId=android&methods=login%20%5B%22test_username%22%2C%20%22test_password%22%2C%201%5D%0A&signature=1.0,cc93095163a83e913c8346a734f9ddc8',
-                       text='ok\'b\n["test_username", "avatar.gif", "John Smith", 8889, "M"]',
-                       cookies={'_art_userid': 1231432523})
-
     sut = FilmwebApi('test_username', 'test_password')
-    sut.login()
+
+    _login(sut, requests_mock)
 
     assert sut.is_logged_in() is True
 
@@ -57,3 +63,20 @@ def test_login_no_credentials(username, password):
 
     with pytest.raises(WrongArgumentException):
         sut.login()
+
+
+def test_get_user_films_want_to_see(requests_mock):
+    sut = FilmwebApi('test_username', 'test_password')
+
+    _login(sut, requests_mock)
+
+    expected_result = [1567198086181,[663821,1403959174000,5,0],[756,1397549920000,5,0]]
+
+    requests_mock.get(
+        API_HOST + '?version=1.0&appId=android&methods=getUserFilmsWantToSee%20%5B8889%2C%201%5D%0A&signature=1.0,d43b89bb43a4fea3be7921e8783b4856',
+        text='ok\'b\n[1567198086181,[663821,1403959174000,5,0],[756,1397549920000,5,0]] s\n',
+        cookies={})
+
+    films = sut.get_user_films_want_to_see()
+
+    assert expected_result == films
